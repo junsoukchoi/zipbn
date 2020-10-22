ee09ec6cada5e39c0f47dadae2f47386997e4300
Junsouk Choi <jchoi@stat.tamu.edu>
2020-10-21 21:28:22 -0500

    Create boxplots of the simulation results (will be used in README.md)

--- evaluation/eval_zero25pct.R
+++ evaluation/eval_zero25pct.R
@@ -76,8 +76,10 @@ for (iter in 1 : n_sim)
 }
 
 # visualize the simulation results
+png(file = "figures/zeros-25pct.png", width = 800, height = 400)
 par(mfrow = c(1, 3), oma = c(0, 0, 2, 0))
 boxplot(ZIPBN$TPR, ODS$TPR, MRS$TPR, main = "TPR")
 boxplot(ZIPBN$FDR, ODS$FDR, MRS$FDR, main = "FDR")
 boxplot(ZIPBN$MCC, ODS$MCC, MRS$MCC, main = "MCC")
-mtext("Simulations with ~25% zeros", outer = TRUE, cex = 1.2)
+mtext("(a) Simulations with ~25% zeros", outer = TRUE, cex = 1.2)
+dev.off()
